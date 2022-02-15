3cc49004b137779289e9b089667237e62c636b39
B. Kerem KURT <kkurt@sabanciuniv.edu>
2022-02-15 11:34:44 +0300

    approval card

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,22 +1,22 @@
 import React from "react";
 import faker from "faker";
 
-const CommentDetail = () => {
+const CommentDetail = (props) => {
   return (
       <div className="comment">
         <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.image()} />
+          <img alt="avatar" src={props.avatar} />
         </a>
         <div className="content">
           <a href="/" className="author">
-            Uncle Sam
+              {props.author}
           </a>
           <div className="metadata">
               <span className="date">
-                Today at 6:00PM
+                {props.timeAgo}
               </span>
           </div>
-          <div className="text">Nice blog post!</div>
+          <div className="text">{props.content}</div>
         </div>
       </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
       <div className="ui container comments">
-          <CommentDetail />
-          <CommentDetail />
-          <CommentDetail />
+          <ApprovalCard />
+          <CommentDetail author="Sam" timeAgo="Today at 3:45PM" content="Nice blog post" avatar={faker.image.image()} />
+          <CommentDetail author="Alex" timeAgo="Today at 2:00PM" content="I like it" avatar={faker.image.image()} />
+          <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" content="I like writing" avatar={faker.image.image()} />
       </div>
   );
 };
