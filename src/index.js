041372e477b77b29cb2f720b4cd5c266d02320d0
B. Kerem KURT <kkurt@sabanciuniv.edu>
2022-02-15 13:15:08 +0300

    lat retrived

--- src/index.js
+++ src/index.js
@@ -1,28 +1,26 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from "./CommentDetail";
-import ApprovalCard from "./ApprovalCard";
 
-const App = () => {
-  return (
-      <div className="ui container comments">
-          <ApprovalCard>
-              <div>
-                  <h4>Warning!</h4>
-                  Are you sure you want to do this?
-              </div>
-          </ApprovalCard>
-          <ApprovalCard>
-              <CommentDetail author="Sam" timeAgo="Today at 3:45PM" content="Nice blog post" avatar={faker.image.image()} />
-          </ApprovalCard>
-          <ApprovalCard>
-              <CommentDetail author="Alex" timeAgo="Today at 2:00PM" content="I like it" avatar={faker.image.image()} />
-          </ApprovalCard> <ApprovalCard>
-              <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" content="I like writing" avatar={faker.image.image()} />
-          </ApprovalCard>
-      </div>
-  );
-};
+class App extends React.Component {
+    constructor(props) {
+        super(props);
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+        this.state = { lat: null};
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({ lat: position.coords.latitude });
+            },
+            (err) => console.log(err)
+        );
+    }
+
+    render() {
+        return <div>Latitude: {this.state.lat}</div>;
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
