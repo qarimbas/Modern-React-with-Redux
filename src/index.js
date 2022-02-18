6e84d64a115e42c2e8a1401b635fb93bc6c372ee
B. Kerem KURT <kkurt@sabanciuniv.edu>
2022-02-18 14:39:12 +0300

    search project

--- src/index.js
+++ src/index.js
@@ -1,46 +1,17 @@
-import React from "react";
+import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from "./SeasonDisplay";
-import Spinner from "./Spinner";
-
-class App extends React.Component {
-    state = {lat: null, errorMessage: ''};
-
-    componentDidMount() {
-        console.log('My component was rendereed to the screen')
-
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => this.setState({lat: position.coords.latitude}),
-            (err) => this.setState({errorMessage: err.message}),
-        );
-    }
-
-    componentDidUpdate() {
-        console.log('My component was just updated!')
-    }
-
-    renderContent() {
-        if (this.state.errorMessage && !this.state.lat) {
-            return <div>Error: {this.state.errorMessage}</div>;
-        }
-        if (!this.state.errorMessage && this.state.lat) {
-            return <SeasonDisplay lat={this.state.lat}/>
-        }
-
-        return <Spinner message="Please accept location request"/>;
-    }
-
-
-    render() {
-        return (
-            <div className="border red">
-                {this.renderContent()}
-            </div>
-        );
-    }
-}
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
