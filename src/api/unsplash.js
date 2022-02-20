991adeeb5c4254634b5ff48a31414e1eb6999409
B. Kerem KURT <kkurt@sabanciuniv.edu>
2022-02-20 11:19:37 +0300

    videoapp started

--- src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: 'https://api.unsplash.com',
-    headers: {
-        Authorization: 'Client-ID ObO4Og1oimmTg_DR3Wx_sGQOdw724CqSuFmcOCu7tz0'
-    }
-});
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,31 +1,8 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-    onInputChange(event) {
-        console.log(event.target.value);
-    }
-
-    state = {term: ''};
-
-    onFormSubmit = (event) => {
-        event.preventDefault();
-
-        this.props.onSubmit(this.state.term);
-    }
-
     render() {
-        return (<div className="ui segment">
-                <form onSubmit={this.onFormSubmit} className="ui form">
-                    <div className="field">
-                        <label>Image Search</label>
-                        <input type="text" value={this.state.term}
-                               onChange={(e) => {
-                            this.setState({term: e.target.value})
-                        }}/>
-                    </div>
-                </form>
-         </div>
-        );
+        return <div>SearchBar</div>;
     }
 }
 
