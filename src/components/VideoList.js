a3eb70e5e83c199c520e011f5e3e0d5ad6b14f7a
B. Kerem KURT <kkurt@sabanciuniv.edu>
2022-02-20 13:25:33 +0300

    list css fixed

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -6,7 +6,9 @@ const VideoList = ( {videos} ) => {
     return <VideoItem video={video} />;
   });
 
-  return <div>{renderedList}</div>;
+  return <div className="ui relaxed divided list">
+    {renderedList}
+  </div>;
 };
 
 export default VideoList;
