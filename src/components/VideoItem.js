ac7b0b2f1a3cb73cbbbd422c06a1e7efb3f6696b
B. Kerem KURT <kkurt@sabanciuniv.edu>
2022-02-21 20:29:50 +0300

    select video

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,8 +1,10 @@
 import React from "react";
 import './VideoItem.css';
 
-const VideoItem = ({video}) => {
-  return (<div className="video-item item">
+const VideoItem = ({video, onVideoSelect}) => {
+  return (<div onClick={() => {
+    onVideoSelect(video)
+  }} className="video-item item">
     <img className="ui image" src={video.snippet.thumbnails.medium.url}/>
     <div className="content">
       <div className="header">{video.snippet.title}</div>
