38f834d1c8706e7d5f42b9aefb642365308e6398
B. Kerem KURT <kkurt@sabanciuniv.edu>
2022-02-26 12:12:50 +0300

    search timeout

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,11 +2,12 @@ import React, {useState, useEffect} from "react";
 import axios from 'axios';
 
 const Search = () => {
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('programming');
+    const [results, setResults] = useState([]);
 
     useEffect(() => {
         const search = async () => {
-                await axios.get('https://en.wikipedia.org/w/api.php',
+                const {data} = await axios.get('https://en.wikipedia.org/w/api.php',
                     {
                         params: {
                             action: 'query',
@@ -15,12 +16,36 @@ const Search = () => {
                             format: 'json',
                             srsearch: term
                         }
-                    }
-                    );
+                    });
+
+                setResults(data.query.search);
             };
 
-        search();
+            const timeoutId = setTimeout(() => {
+                if(term) {
+                    search();
+                }
+            }, 500);
+
+            return () => {
+                clearTimeout(timeoutId);
+            };
     }, [term]);
+
+    const renderedResults = results.map((result) => {
+        return <div key={result.pageid} className="item">
+            <div className="right floated content">
+                <a href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                   className="ui button">Go</a>
+            </div>
+            <div className="content">
+                <div className="header">
+                    {result.title}
+                </div>
+                <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+            </div>
+        </div>
+    });
 
     return (
         <div>
@@ -32,6 +57,9 @@ const Search = () => {
                         onChange={(e) => setTerm(e.target.value)}
                         className="input" />
                 </div>
+            </div>
+            <div className="ui called list">
+                {renderedResults}
             </div>
         </div>
     );
