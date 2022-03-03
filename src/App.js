0fb5e70f9206043c6879b0281af749debb7fcdff
B. Kerem KURT <kkurt@sabanciuniv.edu>
2022-03-03 19:52:57 +0300

    routing wo react rpouter

--- src/App.js
+++ src/App.js
@@ -34,7 +34,36 @@ const options = [
   }
 ];
 
+const showAccordion = () => {
+  if (window.location.pathname === '/') {
+    return <Accordion items={items} />;
+  }
+};
+
+const showList = () => {
+  if (window.location.pathname === '/list') {
+    return <Search />;
+  }
+};
+
+const showDropdown = () => {
+  if (window.location.pathname === '/dropdown') {
+    return <Dropdown />;
+  }
+};
+
+const showTranslate = () => {
+  if (window.location.pathname === '/translate') {
+    return <Translate />;
+  }
+};
+
 export default () => {
 
-  return <div><Translate /></div>
+  return <div>
+    {showAccordion()}
+    {showList()}
+    {showDropdown()}
+    {showTranslate()}
+  </div>
 };
