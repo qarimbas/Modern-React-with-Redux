062e05ada2a8c7100a056e0ae4bc04e55281e828
B. Kerem KURT <kkurt@sabanciuniv.edu>
2022-03-03 21:47:40 +0300

    useVideos custom hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,30 @@
 import React, {useEffect, useState} from "react";
 import SearchBar from "./SearchBar";
-import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
+import useVideos from "../hooks/useVideos";
 
 const App = () => {
-  const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
+  const [videos, search] = useVideos('qarimbasgta');
 
-  useEffect(() => {
-      onTermSubmit('qarimbasgta');
-  }, []);
+    useEffect(() => {
+        setSelectedVideo(videos[0]);
+    }, [videos]);
 
-    const onTermSubmit = async (term) => {
-        const response = await youtube.get("/search", {
-            params: {
-                q: term,
-            },
-        });
-
-        setVideos(response.data.items);
-        setSelectedVideo(response.data.items[0]);
-
-        this.setState({
-            videos: response.data.items,
-            selectedVideo: response.data.items[0]
-        });
-    };
-
-    const onVideoSelect = (video) => {
-        setSelectedVideo(video);
-    };
+    //selectedVideo: response.data.items[0]
 
     return (
         <div className="ui container">
-            <SearchBar onFormSubmit={onTermSubmit} />
+            <SearchBar onFormSubmit={search} />
             <div className="ui grid">
                 <div className="ui row">
                     <div className="eleven wide column">
                         <VideoDetail video={selectedVideo} />
                     </div>
                     <div className="five wide column">
-                        <VideoList onVideoSelect={onVideoSelect}
+                        <VideoList
+                            onVideoSelect={setSelectedVideo}
                                    videos={videos} />
                     </div>
                 </div>
